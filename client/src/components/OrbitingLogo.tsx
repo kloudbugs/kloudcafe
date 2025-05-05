2d82b43c5eeea7c1b9323f4eb3060b737854c71b
kloudbugs <kloudbugs@gmail.com>
2025-05-05 10:11:28 +0000

    Automatic commit for admin2307-05-05 (5)

--- client/src/components/OrbitingLogo.tsx
+++ client/src/components/OrbitingLogo.tsx
@@ -2,6 +2,7 @@ import React, { useRef, useEffect, useState } from 'react';
 import { useFrame } from '@react-three/fiber';
 import { Html } from '@react-three/drei';
 import * as THREE from 'three';
+import './OrbitingLogo.css';
 
 interface OrbitingLogoProps {
   radius?: number;
